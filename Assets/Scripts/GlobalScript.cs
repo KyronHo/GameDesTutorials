3d0e4204bac62b42689f6c7e4a8c04e1e2efe562
KyronHo <KyronCHo@gmail.com>
2015-05-03 17:55:07 +1000

    GlobalScript Comments
    Added a comment

--- Assets/Scripts/GlobalScript.cs
+++ Assets/Scripts/GlobalScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class GlobalScript : MonoBehaviour {
 
+	//class to enable communication between controllers and the UI
 	public ZombieController zombiecontroller;
 	public UIUpdater uiupdater;
 	public Life1Control life1control;
